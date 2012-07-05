ea8037005525919621c8c6e7af4fbb14d39a02e3
Ivaylo Gochkov <igochkov@gmail.com>
2012-07-05 23:23:53 +0200

    Generated jQuery.UI with ButtonSet

---
+++ jQuery.UI/Widgets/ButtonSet/ButtonSetEvents.cs
@@ -0,0 +1,21 @@
+// ButtonSetEvents.cs
+// Script#/Libraries/jQuery/UI
+// This source code is subject to terms and conditions of the Apache License, Version 2.0.
+//
+
+using System;
+using System.Runtime.CompilerServices;
+
+namespace jQueryApi.UI.Widgets {
+
+    [Imported]
+    [IgnoreNamespace]
+    [NamedValues]
+    public enum ButtonSetEvents {
+
+        /// <summary>
+        /// This event is triggered when the buttonset is created.
+        /// </summary>
+        Create
+    }
+}
