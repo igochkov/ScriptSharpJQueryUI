d44a2146a77dfedd3f2610007bd0f7815ed610d9
Ivaylo Gochkov <igochkov@gmail.com>
2012-07-08 20:33:27 +0200

    Re-generated jQuery.UI

--- jQuery.UI/WidgetType.cs
+++ jQuery.UI/WidgetType.cs
@@ -3,13 +3,16 @@
 // This source code is subject to terms and conditions of the Apache License, Version 2.0.
 //
 
+using System;
 using System.Runtime.CompilerServices;
 
 namespace jQueryApi.UI {
+
     [Imported]
     [IgnoreNamespace]
     [ScriptName("$.ui")]
     public sealed class WidgetType {
+
          private WidgetType() {
          }
          
--- jQuery.UI/jQueryUI.cs
+++ jQuery.UI/jQueryUI.cs
@@ -3,13 +3,16 @@
 // This source code is subject to terms and conditions of the Apache License, Version 2.0.
 //
 
+using System;
 using System.Runtime.CompilerServices;
 
 namespace jQueryApi.UI {
+
     [Imported]
     [IgnoreNamespace]
     [ScriptName("$")]
     public static class jQueryUI {
+
         /// <summary>
         /// Create stateful jQuery plugins using the same abstraction that all jQuery UI widgets.
         /// </summary>
